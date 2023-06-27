65604854ae305029c42429ff0ecbe69e653ec1e2
phamquanghien <pqh.cntt@gmail.com>
2023-06-27 14:54:40 +0700

    CreateProject

---
+++ NewApp/Models/YourClass.cs
@@ -0,0 +1,10 @@
+namespace NewApp.Models
+{
+    public class YourClass
+    {
+        public void MyMethod(string parameter)
+        {
+            //Các câu lệnh của phương thức
+        }
+    }
+}
--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Collections;
+using NewApp.Models;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ArrayList arrList = new ArrayList();
+        for(int i = 0; i < 3; i++)
+        {
+            Person ps = new Person();
+            ps.EnterData();
+            arrList.Add(ps);
+        }
+        // foreach(Person ps in arrList)
+        // {
+        //     ps.Display();
+        // }
+        for(int i = 0; i < 3; i++)
+        {
+            Person ps = (Person) arrList[i];
+            ps.Display();
+        }
+        // List<Person> list = new List<Person>();
+        // for(int i = 0; i < 3; i++)
+        // {
+        //     Person ps = new Person();
+        //     ps.EnterData();
+        //     list.Add(ps);
+        // }
+        // foreach(Person ps in list)
+        // {
+        //     ps.Display();
+        // }
+        List<int> intList = new List<int>();
+        intList.Add(1);
+    }
+}
